0db703144c972cf3ed6b1e712ac3faf39eb9ea27
James Grenning <james.grenning.net>
2014-03-02 15:41:57 -0600

    Added example HW dependent Action implementation.

--- BurnerControlActions.c
+++ BurnerControlActions.c
@@ -15,3 +15,38 @@ void BurnerControl_Action_StopLedAnnimation(struct BurnerControl * burner)
 {
     amongOtherThingsAccessHw();
 }
+
+void BurnerControl_Action_LedOn(struct BurnerControl * self, int ledNumber)
+{
+    amongOtherThingsAccessHw();
+}
+
+void BurnerControl_Action_AllLedsOff(struct BurnerControl * self)
+{
+    amongOtherThingsAccessHw();
+}
+
+void BurnerControl_Action_LedUpOne(struct BurnerControl * self)
+{
+    amongOtherThingsAccessHw();
+}
+
+void BurnerControl_Action_LedDownOne(struct BurnerControl * self)
+{
+    amongOtherThingsAccessHw();
+}
+
+void BurnerControl_Action_SetPowerTo(struct BurnerControl * self, int powerLevel)
+{
+    amongOtherThingsAccessHw();
+}
+
+void BurnerControl_Action_PowerUpOneLevel(struct BurnerControl * self)
+{
+    amongOtherThingsAccessHw();
+}
+
+void BurnerControl_Action_PowerDownOneLevel(struct BurnerControl * self)
+{
+    amongOtherThingsAccessHw();
+}
