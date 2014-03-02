f853d27a013edb845edcd200bb8f45ace0e58c0b
James Grenning <james.grenning.net>
2014-03-02 15:21:31 -0600

    Action functions added to header changes result in linker errors

--- BurnerControl.h
+++ BurnerControl.h
@@ -21,8 +21,10 @@ void BurnerControl_Action_StopLedAnnimation(struct BurnerControl *);
 void BurnerControl_Action_LedOn(struct BurnerControl *, int ledNumber);
 void BurnerControl_Action_AllLedsOff(struct BurnerControl *);
 void BurnerControl_Action_LedUpOne(struct BurnerControl *);
+void BurnerControl_Action_LedDownOne(struct BurnerControl *);
 
 void BurnerControl_Action_SetPowerTo(struct BurnerControl *, int powerLevel);
 void BurnerControl_Action_PowerUpOneLevel(struct BurnerControl *);
+void BurnerControl_Action_PowerDownOneLevel(struct BurnerControl *);
 
 #endif
