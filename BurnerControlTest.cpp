101d881d67de0ed822a5577411b020e5016fa812
James Grenning <james.grenning.net>
2014-03-02 15:18:54 -0600

    initial test failure for medium high

--- BurnerControlTest.cpp
+++ BurnerControlTest.cpp
@@ -83,3 +83,17 @@ TEST(BurnerControlStartState, medium_low_power)
     BurnerControl_UpPressed(burnerControl);
 }
 
+TEST(BurnerControlStartState, medium_high_power)
+{
+    mock("burner").expectOneCall("BurnerControl_Action_StartLedAnnimation");
+    mock("burner").expectOneCall("BurnerControl_Action_StopLedAnnimation");
+    mock("burner").expectOneCall("BurnerControl_Action_LedOn").withParameter("ledNumber", 8);
+    mock("burner").expectOneCall("BurnerControl_Action_SetPowerTo").withParameter("powerLevel", 100);
+    mock("burner").expectOneCall("BurnerControl_Action_LedDownOne");
+    mock("burner").expectOneCall("BurnerControl_Action_PowerDownOneLevel");
+
+    BurnerControl_PowerButtonPressed(burnerControl);
+    BurnerControl_UpPressed(burnerControl);
+    BurnerControl_DownPressed(burnerControl);
+}
+
